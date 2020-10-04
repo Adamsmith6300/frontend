616b19917011e810bc7daf759fd9860c66a76d11
Adamsmith6300 <Adamsmith6300@gmail.com>
2020-10-04 11:51:35 -0700

    rm .next from git

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_API_URL: 'https://api.shoploma.ca',
+    NEXT_PUBLIC_API_URL: "https://api.shoploma.ca",
+    NEXT_PUBLIC_PRODUCT_IMAGE_URL:
+      "https://loma-product-images.s3.amazonaws.com",
   },
-}
+};
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,64 @@
+import { useEffect } from "react";
 import Layout from "../components/hoc/layout";
 import { connect } from "react-redux";
-import { wrapper } from "../store";
+// import { wrapper } from "../store";
+import actions from "../store/actions";
+
 import { Button } from "semantic-ui-react";
 
-import actions from "../store/actions";
-import axios from "axios";
+const Page = ({ getProducts, products }) => {
+  if (products != undefined && products.length > 0) {
+    products = products.map((product, index) => {
+      let mainImageUrl = `${process.env.NEXT_PUBLIC_PRODUCT_IMAGE_URL}/${
+        product.MerchantId
+      }/${product.ProductId}/${product.images[product.mainImage]}`;
 
-const Page = (props) => {
-  // if (products != undefined && products.length > 0) {
-  //   products = products.map((product, index) => {
-  //     return (<li key={product.ProductId}>{product.title}</li>)
-  //   })
-  // }
-  console.log("SERVER SIDE PROPS:", props);
+      return (
+        <div
+          key={product.ProductId}
+          className="border border-grey-300 p-3 w-300"
+        >
+          <img src={mainImageUrl} className="w-full" />
+          {product.title}
+        </div>
+      );
+    });
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <Layout>
-      <h1 className="text-3xl text-center">LOMA</h1>
-      <ul>{/* {products} */}</ul>
+      <h1 className="text-3xl text-center">Products</h1>
+      <div className="w-4/5 mx-auto">{products}</div>
     </Layout>
   );
 };
 
-export const getStaticProps = wrapper.getStaticProps(({ store, preview }) => {
-  axios
-    .get(`${process.env.NEXT_PUBLIC_API_URL}/market/products`)
-    .then(function (response) {
-      store.dispatch({
-        type: actions.GET_PRODUCTS,
-        payload: response.data,
-      });
-    })
-    .catch(function (error) {
-      store.dispatch({
-        type: actions.ERROR,
-        payload: "FAILED TO GET PRODUCTS",
-      });
-    });
-});
+// export const getStaticProps = wrapper.getStaticProps(({ store, preview }) => {
+//   axios
+//     .get(`${process.env.NEXT_PUBLIC_API_URL}/market/products`)
+//     .then(function (response) {
+//       store.dispatch({
+//         type: actions.GET_PRODUCTS,
+//         payload: response.data,
+//       });
+//     })
+//     .catch(function (error) {
+//       store.dispatch({
+//         type: actions.ERROR,
+//         payload: "FAILED TO GET PRODUCTS",
+//       });
+//     });
+// });
 
 const mapDispatchToProps = (dispatch) => {
-  return { setTest: (val) => dispatch(actions.setTest(val)) };
+  return {
+    setTest: (val) => dispatch(actions.setTest(val)),
+    getProducts: () => dispatch(actions.getProducts()),
+  };
 };
 
 export default connect((state) => state, mapDispatchToProps)(Page);
