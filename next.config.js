de8ea0b98a957d1cff4d73275a5ae0aab92e6656
Adamsmith6300 <Adamsmith6300@gmail.com>
2021-07-26 16:59:14 -0700

    envs changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_API_URL: "https://api.dev.shoploma.ca",
+    NEXT_PUBLIC_API_URL: "https://api.shoploma.dev",
     NEXT_PUBLIC_MERCHANT_IMAGE_URL:
       "https://loma-merchant-images-dev.s3.amazonaws.com",
     NEXT_PUBLIC_MARKET_IMAGE_URL:
@@ -9,8 +9,8 @@ module.exports = {
       "pk_test_51HZ6tJCTNHGxwAKVcquxotTwADz0yD8njLfQIOPEi0lhr2ljF4WWv9owIFTKcz8wROwSMCki5GvCJg8XxaT691ZB00FS7dghlt",
     NEXT_PUBLIC_SHOPIFY_API_KEY: "f7507170fd60a142f2492150bc998c79",
     NEXT_PUBLIC_COGNITO_CLIENT_ID: "7u26j8tu1169n8s7sp3v4kc1vv",
-    NEXT_PUBLIC_COGNITO_DOMAIN: "https://auth.dev.shoploma.ca/oauth2/authorize",
-    NEXT_PUBLIC_DOMAIN: "https://dev.shoploma.ca",
-    NEXT_PUBLIC_AUTH_DOMAIN: "https://auth.dev.shoploma.ca",
+    NEXT_PUBLIC_COGNITO_DOMAIN: "https://auth.shoploma.dev/oauth2/authorize",
+    NEXT_PUBLIC_DOMAIN: "https://shoploma.dev",
+    NEXT_PUBLIC_AUTH_DOMAIN: "https://auth.shoploma.dev",
   },
 };
