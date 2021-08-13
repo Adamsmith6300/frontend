46914aa0771bb394f82ca671ff61088bffaff6ab
Adamsmith6300 <Adamsmith6300@gmail.com>
2021-08-13 13:46:58 -0700

    prod updates

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     NEXT_PUBLIC_STRIPE_API_KEY:
       "pk_test_51HZ6tJCTNHGxwAKVcquxotTwADz0yD8njLfQIOPEi0lhr2ljF4WWv9owIFTKcz8wROwSMCki5GvCJg8XxaT691ZB00FS7dghlt",
     NEXT_PUBLIC_SHOPIFY_API_KEY: "f7507170fd60a142f2492150bc998c79",
-    NEXT_PUBLIC_COGNITO_CLIENT_ID: "7u26j8tu1169n8s7sp3v4kc1vv",
+    NEXT_PUBLIC_COGNITO_CLIENT_ID: "72it1qodcvjcnpteklpm22k3s5",
     NEXT_PUBLIC_COGNITO_DOMAIN: "https://auth.shoploma.ca/oauth2/authorize",
     NEXT_PUBLIC_DOMAIN: "https://shoploma.ca",
     NEXT_PUBLIC_AUTH_DOMAIN: "https://auth.shoploma.ca",
