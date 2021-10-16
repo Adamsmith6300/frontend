d751177a207d25c6d29b353fae1fb751cbd59479
Adamsmith6300 <Adamsmith6300@gmail.com>
2021-10-16 11:57:04 -0700

    live stripe key frontend

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     NEXT_PUBLIC_MARKET_IMAGE_URL:
       "https://loma-market-images-prod.s3.amazonaws.com",
     NEXT_PUBLIC_STRIPE_API_KEY:
-      "pk_test_51HZ6tJCTNHGxwAKVcquxotTwADz0yD8njLfQIOPEi0lhr2ljF4WWv9owIFTKcz8wROwSMCki5GvCJg8XxaT691ZB00FS7dghlt",
+      "pk_live_51HZ6tJCTNHGxwAKVLpuTkq9VGoc7aHJnwrfbFf4gUKwcAoJsGR2y023qPXfPnGmy2VKDond8GLKOElihK9CrJALG007LI2ADsx",
     NEXT_PUBLIC_SHOPIFY_API_KEY: "f7507170fd60a142f2492150bc998c79",
     NEXT_PUBLIC_COGNITO_CLIENT_ID: "72it1qodcvjcnpteklpm22k3s5",
     NEXT_PUBLIC_COGNITO_DOMAIN: "https://auth.shoploma.ca/oauth2/authorize",
