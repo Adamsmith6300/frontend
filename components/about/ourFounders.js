2000df9cdd8b317decae5418b9df0cb328b20e3b
Adamsmith6300 <Adamsmith6300@gmail.com>
2021-11-19 18:31:19 -0800

    founders info somewhat done

--- components/about/ourFounders.js
+++ components/about/ourFounders.js
@@ -5,15 +5,24 @@ const index = ({}) => {
         <h2>Our Founders</h2>
         <div className="mt-4 mx-auto px-12 lg:px-24">
           <p className="">
-            Adam is a permaculture design ethusiast and software developer.
-            Early in his career, he travelled through central america studying
-            sustainable design while simultaneously teaching himself web
-            development. He returned from travelling with a permaculture design
-            certificate and enrolled at BCIT to further his software development
-            studies. He has always loved technology and after finding
-            permaculture has constantly dreamt of ways to merge the two.
+            <span className="font-bold">Adam</span> - is a permaculture design
+            ethusiast and software developer. Early in his career, he travelled
+            through central america studying sustainable design while
+            simultaneously teaching himself web development. He returned from
+            travelling with a permaculture design certificate and enrolled at
+            BCIT to further his software development studies. He has always
+            loved technology and after finding permaculture has constantly
+            dreamt of ways to merge the two.
           </p>
-          <p className="mt-4">Talia </p>
+          <p className="mt-4">
+            <span className="font-bold">Talia</span> - grew up on the
+            sunshine-coast of BC. She is a passionate environmentalist and
+            animal lover. She is an MBB gradute from SFU and current graduate
+            student in Resource and Environmental Managment at SFU, with a focus
+            on environmental toxicology. Talia believes that sustainable
+            businesses are an important vehicle to drive sustainable change, now
+            more than ever.
+          </p>
         </div>
       </div>
       <div className="w-full h-500 md:w-1/2 overflow-hidden">
